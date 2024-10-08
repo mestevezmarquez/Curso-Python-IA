1a8f11d130bd45a5fbce36165ca143a3996f42b1
Miguel Estevez Marquez <miguel@MacBook-Air-de-Miguel.local>
2024-10-08 19:35:50 +0200

    Ejemplos de operadores de bits

---
+++ Tema2.py
@@ -0,0 +1,16 @@
+
+op_1 = 4&5 #Compara cada bit de dos operandos y devuelve un 1 si ambos bits son 1, de lo contrario, devuelve 0.
+"""
+4 = 0100
+5 = 0101
+Por lo tanto devuelve 0100
+"""
+print(op_1)
+
+op_2 = 4|5 #Devuelve un 1 si al menos uno de los bits es 1
+print(op_2)
+"""
+4 = 0100
+5 = 0101
+Por lo tanto devuelve 0101 = 5
+"""
