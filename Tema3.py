4f980fc517afc54b13a7dbf5185901c6b5e6d94f
Miguel Angel Estevez Marquez <migest@ctag.com>
2024-10-09 13:31:57 +0200

    Creación fichero Tema3

---
+++ Tema3.py
@@ -0,0 +1,12 @@
+#EJEMPLOS DEL TEMA 3
+
+# Estructuras de datos
+#Listas
+lista = [3,"hola",True]
+lista_vacia = [] #creación de lista vacía
+
+print(lista[2]) #Devuelve el True, el tercer elemento de la lista
+print(lista[0]) #Devuelve 
+
+
+
